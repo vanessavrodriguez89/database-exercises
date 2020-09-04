9ddc0054206246996723d0743b0251f25c8b40fe
Vanessa Rodriguez <vanessa.v.rodriguez89@gmail.com>
2020-09-04 12:18:57 -0500

    refactored code

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,10 +1,11 @@
 USE codeup_test_db;
+DROP TABLE IF EXISTS albums;
 CREATE TABLE albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist VARCHAR(100) NOT NULL,
-    name VARCHAR(100) NOT NULL,
-    release_date DATE NOT NULL,
-    sales INT NOT NULL,
-    genre VARCHAR NOT NULL,
+    artist VARCHAR(100),
+    name VARCHAR(100),
+    release_date INT(4),
+    sales FLOAT(9),
+    genre VARCHAR(300),
     PRIMARY KEY (id)
 );
